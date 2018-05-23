c03806abff3ca348375f132fdcd29c0894a0e48c
ivpsa <ivan_vilugron@hotmail.es>
2018-05-23 09:39:37 -0400

    controlador de bodega

--- app/Http/Controllers/bodegaController.php
+++ app/Http/Controllers/bodegaController.php
@@ -3,6 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\TC_TIPO_CONEXION;
+use App\TI_TIPO_IMPLANTE;
+use App\CLC_COLOR_CODING;
+use App\PRO_PRODUCTOS;
+use App\User;
+use App\Http\Controllers\Controller;
+use Exception;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
 
 class bodegaController extends Controller
 {
