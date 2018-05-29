dc9a5eabf5d5bf818ba04ab23503212c326073c2
ivpsa <ivan_vilugron@hotmail.es>
2018-05-29 16:09:43 -0400

    migracion y modelo de piezas dentales

---
+++ database/migrations/2018_05_28_161623_PD_PIEZAS_DENTALES.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PDPIEZASDENTALES extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('PD_PIEZAS_DENTALES', function (Blueprint $table) {
+            $table->increments('PD_COD');
+            $table->integer('PD_SECTOR');
+            $table->integer('PD_N_DIENTE');
+            $table->string('PD_NOMBRE',45);
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('PD_PIEZAS_DENTALES');
+    }
+}
