99c26d75118c97f8433e7c151ea7e9c2162fe18e
ivpsa <ivan_vilugron@hotmail.es>
2018-07-09 19:44:59 -0400

    conteo por medida y longitud

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\TC_TIPO_CONEXION;
+use App\TI_TIPO_IMPLANTE;
+use App\CLC_COLOR_CODING;
+use App\PRO_PRODUCTOS;
+use App\ART_ARTICULOS;
+use App\User;
+use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     /**
@@ -23,6 +29,115 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+      $implantesDe8x29=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '2.9mm')->where('PROD_LONGITUD', '8mm')->value('PROD_COD');
+
+      $implantesDe8x33=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '3.3mm')->where('PROD_LONGITUD', '8mm')->value('PROD_COD');
+
+
+      $implantesDe8x41=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '4.1mm')->where('PROD_LONGITUD', '8mm')->value('PROD_COD');
+
+      $implantesDe8x48=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '4.8mm')->where('PROD_LONGITUD', '8mm')->value('PROD_COD');
+
+      $implantesDe10x29=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '2.9mm')->where('PROD_LONGITUD', '10mm')->value('PROD_COD');
+
+      $implantesDe10x33=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '3.3mm')->where('PROD_LONGITUD', '10mm')->value('PROD_COD');
+
+      $implantesDe10x41=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '4.1mm')->where('PROD_LONGITUD', '10mm')->value('PROD_COD');
+
+      $implantesDe10x48=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '4.8mm')->where('PROD_LONGITUD', '10mm')->value('PROD_COD');
+
+
+      $implantesDe12x29=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '2.9mm')->where('PROD_LONGITUD', '12mm')->value('PROD_COD');
+
+      $implantesDe12x33=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '3.3mm')->where('PROD_LONGITUD', '12mm')->value('PROD_COD');
+
+      $implantesDe12x41=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+
+      'PRO_PRODUCTOS.PROD_COD')
+      ->where('PROD_DIAMETRO', '4.1mm')->where('PROD_LONGITUD', '12mm')->value('PROD_COD');
+
+      $implantesDe12x48=DB::table('ART_ARTICULOS')
+      ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+      ->select(
+      'PRO_PRODUCTOS.PROD_UDI_01')
+      ->where('PROD_DIAMETRO', '4.8mm')->where('PROD_LONGITUD', '12mm')->value('PROD_COD');
+
+
+//       $implantesde10=DB::table('ART_ARTICULOS')
+//       ->Join('PRO_PRODUCTOS', 'PRO_PRODUCTOS.PROD_COD', '=', 'ART_ARTICULOS.ART_PROD_COD')
+//       ->select(
+//
+//       'PRO_PRODUCTOS.PROD_COD')
+//       ->where('PROD_LONGITUD', '8mm')->orderBy('PRO_PRODUCTOS.PROD_LONGITUD', 'ASC')->get();
+// // dd($implantesde10);
+
+
+
+        return view('home',compact(
+          'implantesDe8x29',
+          'implantesDe8x33',
+          'implantesDe8x41',
+          'implantesDe8x41',
+          'implantesDe8x48',
+          'implantesDe10x29',
+          'implantesDe10x33',
+          'implantesDe10x41',
+          'implantesDe10x48',
+          'implantesDe12x29',
+          'implantesDe12x33',
+          'implantesDe12x41',
+          'implantesDe12x48'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
 
                 <!-- <div class="card-header">Dashboard</div> -->
 
-                <div class="card-body">
+              <div class="card-body">
+
 
                 <h3 class="text-center">Bienvenido {{ Auth::user()->name }} a SM Ware House</h3>
                 <br>
@@ -18,5 +19,112 @@
                       <!-- <a href="{{route('pruebaLector')}}"><button type="button" class="btn btn-dark"> <i class="fa fa-user-md" style="font-size:150px; width:202px;"></i> <br> PRUEBA LECTOR</button></a> -->
 
                 </center>
+                <h4 class="text-center">CONTEO DE IMPLANTES POR RADIO Y LONGITUD</h4>
+                <div class="table-responsive">
+
+                  <table class="table table-bordered" align="center"  id="table">
+                  <tr>
+                    <td width="150">LONGITUD/RADIO</td>
+                    <td class="bg-green">2.9</td>
+                    <td class="bg-green">3.3</td>
+                    <td class="bg-green">4.1</td>
+                    <td class="bg-green">4.8</td>
+                  </tr>
+                  <tr>
+                    <td class="bg-blue">8</td>
+                    <td>
+                    {{
+                      $conteoGeneral = DB::table('ART_ARTICULOS')
+                      ->where('ART_ARTICULOS.ART_PROD_COD',$implantesDe8x29)->sum('ART_ARTICULOS.ART_CANT')
+                    }}
+                    </td>
+                    <td>
+                    {{
+                      $conteoGeneral = DB::table('ART_ARTICULOS')
+                      ->where('ART_ARTICULOS.ART_PROD_COD',$implantesDe8x33)->sum('ART_ARTICULOS.ART_CANT')
+                    }}
+                    </td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',12)->orwhere('ART_ARTICULOS.ART_PROD_COD',3)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',5)->orwhere('ART_ARTICULOS.ART_PROD_COD',17)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="bg-blue">10</td>
+                    <td>
+                    {{
+                      $conteoGeneral = DB::table('ART_ARTICULOS')
+                      ->where('ART_ARTICULOS.ART_PROD_COD',8)->sum('ART_ARTICULOS.ART_CANT')
+                    }}
+                    </td>
+                    <td>
+                    {{
+                      $conteoGeneral = DB::table('ART_ARTICULOS')
+                      ->where('ART_ARTICULOS.ART_PROD_COD',10)->orWhere('ART_ARTICULOS.ART_PROD_COD', 2)->sum('ART_ARTICULOS.ART_CANT')
+                    }}
+                    </td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',13)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',6)->orWhere('ART_ARTICULOS.ART_PROD_COD', 16)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+
+                  </tr>
+                  <tr>
+                    <td class="bg-blue">12</td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',12)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',1)->orWhere('ART_ARTICULOS.ART_PROD_COD',11)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+                    <td>
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',4)->orWhere('ART_ARTICULOS.ART_PROD_COD',7)->orWhere('ART_ARTICULOS.ART_PROD_COD',14)->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+                    </td>
+                    <td>
+
+                      {{
+                        $conteoGeneral = DB::table('ART_ARTICULOS')
+                        ->where('ART_ARTICULOS.ART_PROD_COD',15)->orWhere('ART_ARTICULOS.ART_PROD_COD', '2')->sum('ART_ARTICULOS.ART_CANT')
+                      }}
+
+                    </td>
+                  </tr>
+                  <tr>
+                    <td class="bg-blue">14</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                  </tr>
+                </table>
+                </div>
+              </div>
+
+
 
 @endsection
